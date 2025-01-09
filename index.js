ed5a76aeee852037524e64c4fec50c64944e6b9b
A-SABARISHWARAN <sabarishsrinivas15@gmail.com>
2025-01-09 18:57:52 +0530

    Add files via upload
    check index.js and other files are from npm developers

---
+++ index.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+import {randomSuperhero} from 'superheroes';
+import {randomSupervillain} from 'supervillains';
+
+
+
+var mySuperhero = randomSuperhero();
+var mySupervillain = randomSupervillain();
+
+
+console.log(mySuperhero);
+console.log(mySupervillain);
+
